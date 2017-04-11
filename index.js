84d353291cda7c504cc65b55f343b4425dab8fc0
Hiren Joshi <hirenj@gmail.com>
2017-04-11 20:30:15 +0200

    Fix for non-unique executions

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ try {
 }
 
 const AWS = require('lambda-helpers').AWS;
+const uuid = require('uuid');
 
 if (config.region) {
   require('lambda-helpers').AWS.setRegion(config.region);
@@ -45,8 +46,9 @@ const triggerPostProcess = function(set_path) {
   let params = {
     stateMachineArn: post_process_machine,
     input: JSON.stringify(set_path),
-    name: ((set_path.Key.split('/')[1])+(new Date()).toString()).replace(/[^A-Za-z0-9]/g,'').substring(0,59)
+    name: (uuid.v4().substring(0,10)+(set_path.Key.split('/')[1])).replace(/[^A-Za-z0-9]/g,'').substring(0,59)
   };
+  console.log('Starting execution',params.name);
   return stepfunctions.startExecution(params).promise();
 };
 
